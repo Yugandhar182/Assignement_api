238f6b857d5a250217d1289f681b7110285a50f8
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-10 11:42:34 +0530

    Update +page.svelte

--- src/routes/assignment/+page.svelte
+++ src/routes/assignment/+page.svelte
@@ -1,133 +1,35 @@
 <script>
-    import { onMount , afterUpdate } from 'svelte';
-    import { auth } from '$lib/firebase/firebase.js';
-    import { goto } from '$app/navigation';
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
-    import { getDatabase, ref, onValue,set } from 'firebase/database'; // Import Firebase Realtime Database functions
+  import { onMount, afterUpdate } from 'svelte';
+  import { auth } from '$lib/firebase/firebase.js';
+  import { goto } from '$app/navigation';
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+  import { getDatabase, ref, onValue, push } from 'firebase/database'; // Import Firebase Realtime Database functions
   
-   
-    let email = '';
-    let isBlue = true;
-  
-    onMount(() => {
-      // Create an interval to toggle the color every 2 seconds
-      const interval = setInterval(() => {
-        isBlue = !isBlue;
-      }, 1000);
-  
-      // Clean up the interval when the component is unmounted
-      return () => clearInterval(interval);
-    });
-  
-
-  
-    onMount(() => {
-      // Fetch the user data from Firebase Authentication
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          const userData = {
-         
-            firstname: user.firstName,
-            lastName: user.lastName,
-          };
-          users = [...users, userData];
-        } else {
-          console.log('User is signed out.');
-        }
-      });
-  
-      // Fetch data from Firebase Realtime Database
-      const db = getDatabase();
-      const usersRef = ref(db, 'users');
-  
-      onValue(usersRef, (snapshot) => {
-        if (snapshot.exists()) {
-          // Convert the snapshot value to an array of users
-          const userData = snapshot.val();
-          const userList = Object.keys(userData).map((key) => userData[key]);
-          users = userList;
-        } else {
-          // Handle the case where there is no data
-          console.log('No user data in the database.');
-        }
-      });
-    }); let newUser = {
-    firstName: '',
-    lastName: '',
-  };
-
-  let isModalOpen = false;
-
-  function handleAdd() {
-    // Set isModalOpen to true to open the modal
-    isModalOpen = true;
-  }
-
-  // Function to handle the form submission in the modal
-  function handleSave(event) {
-    event.preventDefault();
-
-    // Perform any necessary validation here
-    addUserToDatabase(newUser);
-    // Assuming you want to add the new user to the Firebase Realtime Database
-    // Create a reference to the database and set the user data
-    const db = getDatabase();
-    const newUserRef = ref(db, 'users/' + newUser.firstName); // You can use a unique identifier as the key
-
-    set(newUserRef, newUser)
-      .then(() => {
-        console.log('User data added to the database.');
-        
-        // Add the new user to the local users array immediately
-        users = [...users, newUser];
-
-        // Clear the newUser object for the next entry
-        newUser = {
-          firstName: '',
-          lastName: '',
-        };
-
-        // Close the modal after saving
-        isModalOpen = false;
-      })
-      .catch((error) => {
-        console.error('Error adding user data:', error);
-      });
-  }
-  
-    let users = [
-    
-        // ... add more users here
-    ];
-    onMount(() => {
-        // Fetch data from Firebase Realtime Database
-        const db = getDatabase();
-        const usersRef = ref(db, 'users'); // Assuming 'users' is the path to your user data in the database
-
-        onValue(usersRef, (snapshot) => {
-            if (snapshot.exists()) {
-                // Convert the snapshot value to an array of users
-                const userData = snapshot.val();
-                const userList = Object.values(userData); // Use Object.values to convert object to an array
-                users = userList;
-            } else {
-                // Handle the case where there is no data
-                console.log('No user data in the data.');
-            }
-        });
-    });
-
-  
+  let users = [];
   let dataGrid;
-  
+
+  // Initialize Firebase
+  const db = getDatabase();
+  const usersRef = ref(db, 'users');
+
   onMount(() => {
-    dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
+    onValue(usersRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        users = Object.values(data);
+        if (!dataGrid) {
+          createDataGrid();
+        }
+      }
+    });
+  });
+
+  const createDataGrid = () => {
+    dataGrid = new DevExpress.ui.dxDataGrid('#dataGrid', {
       dataSource: users,
       columns: [
-     
         { dataField: 'firstName', caption: 'FirstName' },
         { dataField: 'lastName', caption: 'lastName' },
-      
       ],
       showBorders: true,
       filterRow: {
@@ -137,18 +39,18 @@
         allowDeleting: true,
         allowAdding: true,
         allowUpdating: true,
-        mode: "popup",
+        mode: 'popup',
         form: {
-          labelLocation: "top",
+          labelLocation: 'top',
         },
         popup: {
           showTitle: true,
         },
         texts: {
-          saveRowChanges: "Save",
-          cancelRowChanges: "Cancel",
-          deleteRow: "Delete",
-          confirmDeleteMessage: "Are you sure you want to delete this record?",
+          saveRowChanges: 'Save',
+          cancelRowChanges: 'Cancel',
+          deleteRow: 'Delete',
+          confirmDeleteMessage: 'Are you sure you want to delete this record?',
         },
       },
       paging: {
@@ -159,49 +61,26 @@
         allowedPageSizes: [10, 20],
         showInfo: true,
       },
-      
+      // Event handler for when a new row is added
+      onRowInserted: (e) => {
+        const newData = e.data;
+        // Push the new data to Firebase
+        push(usersRef, newData);
+      },
     });
-    });
+  };
 
-    afterUpdate(() => {
-        if (dataGrid) {
-            dataGrid.option('dataSource', users);
-        }
-    });
-
-    function addUserToDatabase(newUser) {
-        const db = getDatabase();
-        const newUserRef = push(ref(db, 'users')); // Use push to generate a new unique key
-
-        set(newUserRef, newUser)
-            .then(() => {
-                console.log('User data added to the database.');
-
-                // No need to manually update the local users array; it will be updated automatically when Firebase data changes.
-
-                // Clear the newUser object for the next entry
-                newUser = {
-                    firstName: '',
-                    lastName: '',
-                };
-
-                // Close the modal after saving
-                isModalOpen = false;
-            })
-            .catch((error) => {
-                console.error('Error adding user data:', error);
-            });
+  afterUpdate(() => {
+    if (!dataGrid && users.length > 0) {
+      createDataGrid();
     }
+  });
+</script>
 
-  
-  </script>
-  
-  <main>
-    <div id="dataGrid"></div>
-  </main>
- 
-  
-  
-  <style>
-   
-  </style>
+<main>
+  <div id="dataGrid"></div>
+</main>
+
+<style>
+  /* Add your styles here if needed */
+</style>
